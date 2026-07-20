9f7ed0a40c055fa179ff4f2a287c79925fd3fa58
annielca <annieliao1230@gmail.com>
2026-07-20 14:48:40 +0800

    Revert "Rename train_set to training_set"
    This reverts commit fb609bc85798bb69c38110484bc01726f412a61f.

--- data.py
+++ data.py
@@ -23,8 +23,9 @@ def get_dataloaders(batch_size: int = 64, root: str = "./data"):
         transforms.Normalize((0.1307,), (0.3081,)),
     ])
 
-    training_set = MNIST(root=root, train=True, download=True, transform=train_tf)
+    train_set = MNIST(root=root, train=True, download=True, transform=train_tf)
     test_set = MNIST(root=root, train=False, download=True, transform=test_tf)
+
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_set, batch_size=128, shuffle=False)
     return train_loader, test_loader
